19e2b12e2dc52239e4564e3a582069b73d9daaa9
GabrielMarllos <u202121584@upc.edu.pe>
2024-10-22 00:38:37 -0500

    (feat)Link updated

--- lib/services/config.dart
+++ lib/services/config.dart
@@ -1,3 +1,3 @@
 class Config {
-  static const String baseUrl = "https://drivesafepresentation20241020133638.azurewebsites.net";
+  static const String baseUrl = "https://drivesafepresentation.azurewebsites.net";
 }
